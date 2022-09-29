99c2a334dd2553bcb9e1817de29df0ad34c56a7a
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-09-29 11:33:04 -0500

    seller ofthe month, single listing html and dynamic variables, displaying data in templates, pagination

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    
+    # to display with page numbers
     paginator = Paginator(listings, 3)
     page =request.GET.get('page')
     paged_listings = paginator.get_page(page)
@@ -18,7 +18,12 @@ def index(request):
     return render(request, 'listings/listings.html',context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk =listing_id)
+    
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
     
 def search(request):
