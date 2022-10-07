20b1d46c1dd59e5bf2efb19639b9d07cdcd5b42d
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-10-07 13:32:33 -0500

    email config gmail

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+
+#Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'testingemail9872@gmail.com'
+EMAIL_HOST_PASSWORD = 'testingemail'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
+
 
 def contact(request):
     if request.method == 'POST':
@@ -23,6 +25,16 @@ def contact(request):
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         
         contact.save()
+        
+        #send email
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing + '. Sign in to the panel for more information',
+            'testingemail9872@gmail.com',
+            [realtor_email, 'jzchoudrie@gmail.com'],
+            fail_silently = False,        
+             
+        )
         
         messages.success( request, 'Your request has been submitted, a realtor will get back to you')
         
