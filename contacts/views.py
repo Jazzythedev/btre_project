6603b7e49229a992121b3eed265531fbfe93f6bd
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-10-07 14:12:58 -0500

    logic to check if logged in user is making inquiry about same house

--- contacts/views.py
+++ contacts/views.py
@@ -13,6 +13,13 @@ def contact(request):
         user_id = request.POST['user_id']        
         realtor_email = request.POST['realtor_email']
         
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_connected = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_connected:
+                messages.error( request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+        
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         
         contact.save()
