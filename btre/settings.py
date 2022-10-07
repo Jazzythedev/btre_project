19ccd9cfe8efff3a1274d2907e98d6cff0c59064
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-10-07 14:26:42 -0500

    Made dashboard dynamic, changed emsil server to point to email test server, added logic to dashboard to display user inquiries by most recent

--- btre/settings.py
+++ btre/settings.py
@@ -148,9 +148,13 @@ MESSAGE_TAGS = {
 
 
 #Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testingemail9872@gmail.com'
-EMAIL_HOST_PASSWORD = 'testingemail'
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/app-messages'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
