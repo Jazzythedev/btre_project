a7c2567c4c93ac9615a8624a0ffe5791c80c8255
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-10-20 16:08:47 -0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-# first url pattern path here '' refers to /listings. second one is single listings which looks at detail of each listing.
+
 urlpatterns = [
     path( 'login', views.login, name = 'login'),
     path('register', views.register, name = 'register'),
